60aa16224e8d088ef67b62577c2e72e130f00db7
gaelcolas <gaelcolas@users.noreply.github.com>
2016-10-31 16:47:25 +0000

    removing unecessary Application folder in data store creation

--- Tooling/DscConfiguration/New-ConfigurationDataStore.ps1
+++ Tooling/DscConfiguration/New-ConfigurationDataStore.ps1
@@ -9,7 +9,6 @@ function New-DscConfigurationDataStore {
     mkdir (join-path $Path 'Credentials') | out-null
     mkdir (join-path $Path 'Services') | out-null
     mkdir (join-path $Path 'SiteData') | out-null
-    mkdir (join-path $Path 'Applications') | out-null
 
     $script:ConfigurationDataPath = $Path
 }
