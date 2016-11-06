32ed8cb23f64a9689fd75866cde0472e112bea2d
gaelcolas <gaelcolas@users.noreply.github.com>
2016-11-06 10:30:58 +0000

    Editing Command to use the supported MemberOfServices property for resolution

--- Tooling/DscConfiguration/Public/New-DscNodeMetadata.ps1
+++ Tooling/DscConfiguration/Public/New-DscNodeMetadata.ps1
@@ -48,7 +48,7 @@ function New-DscNodeMetadata
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [string[]]
-        $Services = @(),
+        $MemberOfServices = @(),
 
         [switch]
         $Force
